76a6996f27ee48ccc74d5e6addb58539843f7ce5
SandeepThakare <sandythakare9@gmail.com>
2018-06-14 18:22:58 +0530

    serverless testing done

--- serverless.sh
+++ serverless.sh
@@ -5,8 +5,14 @@
 echo 'installing serverless dependancy'
 npm install serverless -g
 
+echo '====================================================================================='
+echo '\n'
+
 echo 'serverless install on your system globally now you can use serverless commands.'
 echo 'You can set project by simply choosing an option - '
+
+echo '====================================================================================='
+echo '\n'
 
 getServerlessApp() {
     echo '1. Deploy simple serverless app \n 2. Serverless app with Demo api with serverless-ofline and CICD with circleci'
@@ -20,8 +26,12 @@ getServerlessApp() {
         serverless create --template aws-nodejs --path "$PROJECT_NAME"
     elif [ $option -eq 2 ]
     then
+        echo '====================================================================================='
+        echo 'Cloneing project\n\n'
         git clone https://github.com/SandeepThakare/aws-cicd-using-circleci.git "$PROJECT_NAME"
         cd $PROJECT_NAME
+        echo '====================================================================================='
+        echo 'Installing dependancies \n\n'
         npm install
     else
         echo "Invalid Key Press.."
